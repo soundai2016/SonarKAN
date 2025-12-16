924bbadfe13d8fe512bb08acdc28c7a93ebd3223
陈孝良 <chenxiaoliang@soundai.com>
2025-12-16 18:16:44 +0800

    update license

--- src/setup.py
+++ src/setup.py
@@ -21,9 +21,9 @@ setup(
     author_email="chenxiaoliang@soundai.com",
     description="Sonar Kolmogorov–Arnold Network Based on GPU or CPU.",
     license="Apache License",    
-    packages=find_packages(),  # Automatically find and include all packages
-    include_package_data=True,  # Include non-code files specified in MANIFEST.in
-    package_data={'': extra_files},  # Including extra files
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': extra_files},
     keywords=[
         "SonarKAN",      
         "Sonar Kolmogorov–Arnold Network",        
@@ -36,14 +36,12 @@ setup(
         'torch',
     ],
     classifiers=[
-        # Add classifiers to categorize your project
-        # See https://pypi.org/classifiers/ for a full list
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.10",
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "Operating System :: POSIX :: Linux",
-        "License :: OSI Approved :: Software License",
+        "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Libraries :: Python Modules",        
     ],
 )
